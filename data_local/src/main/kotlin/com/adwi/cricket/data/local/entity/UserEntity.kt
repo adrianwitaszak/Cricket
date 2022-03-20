cac6f36fd3e03b418ca27ebb04a09788975cd67a
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-03-20 17:07:58 +0000

    implemented user repository

--- data_local/src/main/kotlin/com/adwi/cricket/data/local/entity/UserEntity.kt
+++ data_local/src/main/kotlin/com/adwi/cricket/data/local/entity/UserEntity.kt
@@ -1,9 +1,11 @@
 package com.adwi.cricket.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "user_table")
 data class UserEntity(
+    @PrimaryKey
     val id: String,
     val name: String = "",
     val email: String = "",
