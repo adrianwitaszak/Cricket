239236198dd4b34e965662a302c946263d66f46d
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-04-10 09:30:32 +0100

    refactor

--- feature_auth/src/main/kotlin/com/adwi/cricket/feature/auth/ui/AuthScreen.kt
+++ feature_auth/src/main/kotlin/com/adwi/cricket/feature/auth/ui/AuthScreen.kt
@@ -32,7 +32,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 const val TEST_TAG_AUTH_SIGN_IN_BUTTON = "sign_in_button"
 const val TEST_TAG_AUTH_HEADER = "header"
@@ -47,41 +46,19 @@ fun AuthScreen(
     goHome: () -> Unit,
 ) {
     val snackbarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
-
     val context = LocalContext.current
+
+    val launcher = getSignInLauncherActivity(intent)
     val googleSignInClient = getGoogleSignInClient(
         context = context,
         token = stringResource(R.string.default_web_client_id)
     )
-    val launcher =
-        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                val credential = GoogleAuthProvider.getCredential(account.idToken!!, null)
-                intent(AuthScreenIntent.SignIntWithCredentials(credential))
-            } catch (e: ApiException) {
-                Timber.tag("TAG").w(e, "Google sign in failed")
-            }
-        }
 
-    when (state.loadingState) {
-        is LoadingState.FAILED -> {
-            val message = stringResource(id = R.string.login_failed)
-            LaunchedEffect(true) {
-                coroutineScope.launch {
-                    snackbarHostState.showSnackbar(message)
-                }
-            }
-            Timber.d("AuthScreen - failed")
-        }
-        is LoadingState.SUCCESS -> {
-            goHome()
-            Timber.d("AuthScreen - goHome")
-        }
-        else -> {}
-    }
+    handleAuthLoadingState(
+        state = state.loadingState,
+        snackbarHostState = snackbarHostState,
+        success = goHome,
+    )
 
     Scaffold(
         scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState),
@@ -135,4 +112,41 @@ private fun getGoogleSignInClient(context: Context, token: String): GoogleSignIn
             .build()
 
     return GoogleSignIn.getClient(context, gso)
+}
+
+private fun getSignInLauncherActivity(
+    intent: (AuthScreenIntent) -> Unit,
+) =
+    rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            val account = task.getResult(ApiException::class.java)!!
+            val credential = GoogleAuthProvider.getCredential(account.idToken!!, null)
+            intent(AuthScreenIntent.SignIntWithCredentials(credential))
+        } catch (e: ApiException) {
+            println("Google sign in failed")
+        }
+    }
+
+private fun handleAuthLoadingState(
+    state: LoadingState,
+    snackbarHostState: SnackbarHostState,
+    success: @Composable () -> Unit,
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    when (state) {
+        is LoadingState.FAILED -> {
+            val message = stringResource(id = R.string.login_failed)
+            LaunchedEffect(true) {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(message)
+                }
+            }
+        }
+        is LoadingState.SUCCESS -> {
+            success()
+        }
+        else -> {}
+    }
 }
